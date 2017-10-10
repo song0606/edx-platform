4e43ef2d47ea32565e3524da3e1e1efde748a02b
Matt Tuchfarber <mtuchfarber@edx.org>
2017-10-10 15:08:11 -0400

    Fix tests and styling

--- lms/static/js/program_marketing.js
+++ lms/static/js/program_marketing.js
@@ -1,6 +1,6 @@
-function playVideo(src){
-    console.log(src)
-    document.querySelector("#program_video button").style = "display:none;";
-    document.querySelector("#program_video iframe").style = "display:block;";
-    document.querySelector("#program_video iframe").src = src;
+function playVideo(src) {
+    'use strict';
+    document.querySelector('#program_video button').style = 'display:none;';
+    document.querySelector('#program_video iframe').style = 'display:block;';
+    document.querySelector('#program_video iframe').src = src;
 }
